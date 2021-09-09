ef2a5f1632d5bea3c7dcf23d244b9fcb837f06bb
yihong.zhan <yihong.zhan@58.com>
2021-09-09 15:31:49 +0800

    默认图片

--- testqf/News/NewsCellTableViewCell.m
+++ testqf/News/NewsCellTableViewCell.m
@@ -12,16 +12,19 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imgsrcView;
 @property (weak, nonatomic) IBOutlet UILabel *titleView;
 @property (weak, nonatomic) IBOutlet UILabel *digestView;
+@property (weak, nonatomic) IBOutlet UIImageView *defaultImg;
 @property (weak, nonatomic) IBOutlet UILabel *replyCount;
+//@property (nonatomic, strong) UIImageView *defaultImg;
 @end
 @implementation NewsCellTableViewCell
 
 -(void)setNews:(News *)news{
+//    self.defaultImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"defaultpic"]];
+    [self.defaultImg setImage:[UIImage imageNamed:@"defaultpic"]];
     [self.imgsrcView setImageWithURL:[NSURL URLWithString:news.imgsrc]];
     self.titleView.text = news.title;
     self.digestView.text = news.digest;
     self.replyCount.text = [NSString stringWithFormat:@"%d人回帖",news.replyCount.intValue];
 }
-
 
 @end
